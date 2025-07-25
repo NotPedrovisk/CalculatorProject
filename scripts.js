04ba24eccd291940c989f684e8e36388ebc86a91
NotPedrovisk <not.pedrovisk69@gmail.com>
2025-07-24 21:51:53 -0400

    added input for second number

--- scripts.js
+++ scripts.js
@@ -9,15 +9,23 @@ let buttons = document.querySelectorAll("button");
 buttons.forEach((but)=>{
     but.addEventListener("click", ()=>{
     
+        //prompts user to input first number
         if(operator === "" && !isNaN(but.textContent)){
-        firstNumber += but.textContent
-        console.log(firstNumber)
-        }
-        
+        firstNumber += but.textContent;
+        console.log(firstNumber);
+        };
+    
+        //checks if the button pressed is an operator
         if(OPERATORS.includes(but.textContent)) {
-            operator = but.textContent
-            console.log(operator)
-        }
+            operator = but.textContent;
+            console.log(operator);
+        };
+
+        //lets user input second number after operator is selected
+        if(operator !="" && !isNaN(but.textContent)){
+            secondNumber += but.textContent;
+            console.log(secondNumber)
+        };
 
         //clears calculator
         if(but.textContent == "AC"){
