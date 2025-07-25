f43c561bffc32fb6b502c1c99f702ec9df80f304
NotPedrovisk <not.pedrovisk69@gmail.com>
2025-07-24 21:49:06 -0400

    managed to get first set of numbers and operator selection in

--- scripts.js
+++ scripts.js
@@ -1,23 +1,30 @@
 let operator = "";
 let firstNumber = "";
 let secondNumber = "";
-const NUMBERS ="1234567890"; 
+const NUMBERS ="1234567890";
+const OPERATORS = "+-*/"; 
 
 let buttons = document.querySelectorAll("button");
 
 buttons.forEach((but)=>{
     but.addEventListener("click", ()=>{
-        
-        if(operator == "" && !isNaN(but.textContent)){
+    
+        if(operator === "" && !isNaN(but.textContent)){
         firstNumber += but.textContent
         console.log(firstNumber)
         }
         
+        if(OPERATORS.includes(but.textContent)) {
+            operator = but.textContent
+            console.log(operator)
+        }
+
         //clears calculator
         if(but.textContent == "AC"){
             firstNumber = "";
             operator = "";
             secondNumber = "";
+            console.log("Cleared");
         }
         
     })
